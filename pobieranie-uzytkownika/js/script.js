77a5ad265128f3acf1feea1bf6fd9bc3fadd9c4d
kacpro9 <sterkowiczkacper@gmail.com>
2025-03-19 09:22:32 +0100

    90% done

---
+++ pobieranie-uzytkownika/js/script.js
@@ -0,0 +1,25 @@
+console.log("Pobieranie uzytkownika");
+
+let btnGetUser = document.getElementById('get-user');
+
+const getUser = () => {
+
+    let divDp = document.getElementById('dane-programisty');
+    divDp.innerHTML = '';
+
+    fetch(`https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php`, {
+        mode: 'cors',
+        method: 'GET',
+    })
+        .then(res => res.json())
+        .then(resJSON => {
+            console.log(resJSON);
+
+            divDp.innerText = resJSON;
+        })
+        .catch((error) => {
+            console.error('Error', error);
+        });
+
+}
+btnGetUser.addEventListener('click', getUser);
